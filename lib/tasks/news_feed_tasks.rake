07d0f4922a9fd0e06f0f01a0e361a77c693d4103
David Leung <dleung@quosap.com>
2012-08-08 16:57:00 -0700

    Removed extra files

--- lib/tasks/news_feed_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :news_feed do
-#   # Task goes here
-# end
--- test/dummy/app/models/messages.rb
+++
@@ -1,3 +0,0 @@
-class Messages < ActiveRecord::Base
-  acts_as_news_feedable object_name: "Message Title"
-end
